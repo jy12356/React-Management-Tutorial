297e9fe7938d1d4c17fa377894974e691092e37e
jy12356 <jy12356@naver.com>
2021-02-05 01:29:14 +0900

    create Customer.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,45 @@
 import logo from './logo.svg';
+import React, { Component } from 'react';
+import { render } from '@testing-library/react';
 import './App.css';
+import Customer from './components/Customer'
 
-function App() {
-  return (
-    <div className="gray_background">
-      <img src={logo} lat="logo"/>
-      <h2>Let's develop management system!</h2>
-    </div>
-  );
+const customers=[
+  {
+    'id': 1,
+    'image' :'https://placeimg.com/64/64/1', 
+    'name' : '서지연',
+    'birthday' : '930728',
+    'gender' : '여자',
+    'job' : '취준생',
+
+  },
+  {
+    'id': 2,
+    'image' :'https://placeimg.com/64/64/2', 
+    'name' : '홍길동s',
+    'birthday' : '961112',
+    'gender' : '남자',
+    'job' : '대학생',
+
+  },
+  {
+    'id': 3,
+    'image' :'https://placeimg.com/64/64/3', 
+    'name' : '백승무',
+    'birthday' : '940312',
+    'gender' : '남자',
+    'job' : '취준생',
+
+  }
+]
+function App(){ 
+    return (
+      <div>
+        {customers.map(c =>{ return( <Customer key={c.id} id={c.id} image={c.image} name={c.name} gender={c.gender} job={c.job} /> ) })}
+      </div>
+    );
+ 
 }
 
 export default App;
